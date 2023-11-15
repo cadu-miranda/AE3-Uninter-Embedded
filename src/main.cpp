d78a0e0ea281aa1241717327c99d3fac9779a5f1
cadu-miranda <polomiranda@outlook.com>
2023-11-15 19:10:12 -0300

    Add new dependencies and implement a web server for data reading

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,74 @@
 #include <Adafruit_Sensor.h>
-#include <Arduino.h>
+#include <ArduinoJson.h>
 #include <DHT.h>
+#include <HTTPClient.h>
+#include <WebServer.h>
+#include <WiFi.h>
 
 #define DHT_PIN 26
 #define DHT_TYPE DHT11
 
+const char *SSID = "";     // Your WiFi SSID
+const char *PASSWORD = ""; // Your WiFi Password
+
+char buffer[512];
+
+WebServer server(3490); // Creates a server in port 3490
 DHT dht(DHT_PIN, DHT_TYPE);
 
+// Function prototypes
+
+void handleDataReading(void);
+void addCustomHeaders(void);
+
 void setup(void) {
+
   Serial.begin(9600);
 
-  Serial.println(F("Teste - DHT11"));
-
   dht.begin();
-}
 
-void loop(void) {
+  Serial.print("Connecting to ");
+  Serial.println(SSID);
 
-  delay(2000);
+  WiFi.begin(SSID, PASSWORD);
 
-  float temperature = dht.readTemperature(), humidity = dht.readHumidity();
+  while (WiFi.status() != WL_CONNECTED) {
 
-  if (isnan(temperature) || isnan(humidity)) {
-    Serial.println(F("Falha ao ler dados do sensor."));
-    return;
+    delay(500);
+    Serial.print(".");
   }
 
-  Serial.print(F("Temperatura: "));
-  Serial.print(temperature);
-  Serial.print(F("°C"));
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 
-  Serial.print(F(" | "));
-  Serial.print(F("Umidade: "));
-  Serial.print(humidity);
-  Serial.print(F("%"));
+  server.on("/data",            // URL:
+                                // http://YOUR_ESP-32_IP_ADDRESS/data
+            handleDataReading); // When the user requests this URL, the
+                                // function handleDataReading is called
+
+  server.begin();
+}
+
+void loop(void) { server.handleClient(); }
+
+void handleDataReading(void) {
+
+  float temperature = dht.readTemperature();
+  int humidity = dht.readHumidity();
+
+  sprintf(buffer, "{ \"mcu_data\": {\"temperature\": %.2f, \"humidity\": %d}}",
+          temperature, humidity);
+
+  addCustomHeaders();
+
+  server.send(200, "application/json; charset=utf-8", buffer);
+}
+
+void addCustomHeaders(void) {
+
+  server.sendHeader("Access-Control-Allow-Methods", "GET");
+  server.sendHeader("Access-Control-Allow-Origin", "*");
+  server.sendHeader("Connection", "close");
 }
