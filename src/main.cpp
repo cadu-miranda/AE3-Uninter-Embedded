c8944008ab990be34f4bc09f2d8a2f998827180b
cadu-miranda <polomiranda@outlook.com>
2023-11-15 18:45:46 -0300

    added integration with dht11

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,37 @@
+#include <Adafruit_Sensor.h>
 #include <Arduino.h>
+#include <DHT.h>
 
-#define LED_PIN 2
+#define DHT_PIN 26
+#define DHT_TYPE DHT11
 
-void setup(void) { pinMode(LED_PIN, OUTPUT); }
+DHT dht(DHT_PIN, DHT_TYPE);
+
+void setup(void) {
+  Serial.begin(9600);
+
+  Serial.println(F("Teste - DHT11"));
+
+  dht.begin();
+}
 
 void loop(void) {
 
-  digitalWrite(LED_PIN, HIGH);
-  delay(1000);
+  delay(2000);
 
-  digitalWrite(LED_PIN, LOW);
-  delay(1000);
+  float temperature = dht.readTemperature(), humidity = dht.readHumidity();
+
+  if (isnan(temperature) || isnan(humidity)) {
+    Serial.println(F("Falha ao ler dados do sensor."));
+    return;
+  }
+
+  Serial.print(F("Temperatura: "));
+  Serial.print(temperature);
+  Serial.print(F("°C"));
+
+  Serial.print(F(" | "));
+  Serial.print(F("Umidade: "));
+  Serial.print(humidity);
+  Serial.print(F("%"));
 }
